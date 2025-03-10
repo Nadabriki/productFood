a7ffba02ecb1f63fba087db86fbf88113b90ef1b
Nadabriki <nadabriki251@gmail.com>
2025-03-10 13:15:25 +0100

    version1

---
+++ src/Entity/Personne.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonneRepository::class)]
+class Personne
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column]
+    private ?int $age = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): static
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+    public function __construct($name,$lastname,$age){
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->age = $age;
+    }
+    public function category(): ?string {
+        if($this->age<0){
+            throw new \Exception("invalid age");
+        }
+        elseif ($this->age < 18) {
+            return "mineur";
+        } else {
+            return "majeur";
+        }
+    }
+
+}
